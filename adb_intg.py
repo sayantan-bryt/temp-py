bd0175650fb5c91ebd24efb467f3b8e3b0d1b61f
sayantan-bryt <sayantan@bryt.in>
2024-03-05 09:40:48 +0530

    update filter

--- adb_intg.py
+++ adb_intg.py
@@ -1,5 +1,7 @@
 from subprocess import check_call
-from flask import Flask
+from flask import Flask, request, Response, jsonify
+import requests
+from functools import partial
 
 app = Flask(__name__)
 
@@ -18,6 +20,34 @@ def wifi_off():
         "adb", "shell", "svc", "wifi", "disable"
     ], shell=True)
     return "done"
+
+
+@app.route("/adb", methods=["POST"])
+def adb():
+    body = request.json
+    command = body.get("command")
+    if not command:
+        return Response(f"Invalid {command=}", status=400)
+
+    check_call(command, shell=True)
+    return "done"
+
+
+@app.route("/forward-req", methods=["POST"])
+def forward() -> Response:
+    body: dict = request.json or {}
+    path = body.pop("path")
+    body = body.pop("body", None)
+    method = body.pop("method", "GET").lower()
+    url = f"https://staging.bryt.in/{path}"
+    req = partial(requests.request, method=method, url=url)
+    data: requests.Response
+    if method == "GET":
+        data = req(params=body)
+    else:
+        data = req(json=body)
+
+    return jsonify(data)
 
 
 if __name__ == "__main__":
--- mitm_addon.py
+++ mitm_addon.py
@@ -10,7 +10,15 @@ class ToggleBlock:
         if req.path == "/wifi/off":
             logging.info(f"Intercepted: {req.path=}")
             # !~u /wifi/(on|off)$ -> alone isn't enough since bryt.in urls were passing through
-            ctx.options.set(r"block_list=:~all & !~u /wifi/(on|off)$:444", r"block_list=:~d .*\.bryt\.in:444")
+            ctx.options.set(
+                (
+                    "block_list="
+                    r":~all & !~u /wifi/(on|off)$:444"
+                    # r" & !~u /forward-req$:444"
+                    # r" & !~u /adb$:444"
+                ),
+                r"block_list=:~d .*\.bryt\.in:444",
+            )
             flow.response = http.Response.make(200, content="off", headers={"Server": version.MITMPROXY})
         elif req.path == "/wifi/on":
             logging.info(f"Intercepted: {req.path=}")
